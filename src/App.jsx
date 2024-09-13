365f46dfde5c843863e606d39b1413c5539c6ea5
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-09-13 09:49:23 +0530

    Define router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
 
 export default function App() {
-  return <div>components</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
